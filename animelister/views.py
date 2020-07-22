3f9961abce1253a3063afe7b3b56c78e4e732dac
root <root@localhost.localdomain>
2020-07-22 19:54:57 +0530

    third commit

--- animelister/views.py
+++ animelister/views.py
@@ -8,9 +8,13 @@ def index(request):
     return render(request, 'index.html')
 
 
-def search(request,id):
-
-    anime = Anime.objects.filter(category=id)
+def search(request):
+    var1 = request.GET['query']
+    if var1 == 'Slice of Life':
+        id = 1
+        anime = Anime.objects.filter(category=id)
+      
+   
     print(anime)
 
     return render(request,'results.html',{'query':id,'animes':anime})
